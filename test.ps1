96cfe7a0b94f40011b184dfe75f3b6ee235adf76
Sauceke <saucekedesu@protonmail.com>
2023-06-20 15:33:11 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 curl -L https://trial.dlsite.com/professional/VJ016000/VJ015728_trial.zip -o scs.zip
 Expand-Archive -Path ./scs.zip
-cd "セクロスフィア H体験版"
+cd ./scs/*/
 & .\Trial.exe
 Start-Sleep -Seconds 20
 taskkill /IM Trial.exe
