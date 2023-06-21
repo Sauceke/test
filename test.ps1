c86faa1afb8c1f22ea7914f559d93b27e8899c92
Sauceke <saucekedesu@protonmail.com>
2023-06-21 13:11:31 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,5 +2,5 @@ curl -L https://trial.dlsite.com/professional/VJ016000/VJ015728_trial.zip -o scs
 Expand-Archive -Path ./scs.zip
 cd ./scs/*/
 & .\Trial.exe
-Start-Sleep -Seconds 20
-taskkill /IM Trial.exe
+Start-Sleep -Seconds 60
+cp ./shot.png ../../shot.png
