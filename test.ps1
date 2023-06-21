bf774c8c1424aaaef3b8dcc5598fc29d82ff4ea4
Sauceke <saucekedesu@protonmail.com>
2023-06-21 14:48:34 +0300

    fix cp

--- test.ps1
+++ test.ps1
@@ -3,7 +3,7 @@ Expand-Archive -Path ./scs.zip
 cd ./scs/*/
 curl -L https://github.com/BepInEx/BepInEx/releases/download/v5.4.18/BepInEx_x86_5.4.18.0.zip -o BepInEx32.zip
 Expand-Archive -Path BepInEx32.zip -DestinationPath .
-cp ../../BepInEx .
+cp -Force -Recurse ../../BepInEx .
 & ./Trial.exe
 Start-Sleep -Seconds 60
 cp ./shot.png ../../shot.png
