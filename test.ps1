aa6125be47008d77566088b5223d9a0a10f27404
Sauceke <saucekedesu@protonmail.com>
2023-06-21 14:37:40 +0300

    patch game before running

--- test.ps1
+++ test.ps1
@@ -1,6 +1,9 @@
 curl -L https://trial.dlsite.com/professional/VJ016000/VJ015728_trial.zip -o scs.zip
 Expand-Archive -Path ./scs.zip
 cd ./scs/*/
-& .\Trial.exe
+curl -L https://github.com/BepInEx/BepInEx/releases/download/v5.4.18/BepInEx_x86_5.4.18.0.zip -o BepInEx32.zip
+Expand-Archive -Path BepInEx32.zip -DestinationPath .
+cp ../../BepInEx .
+& ./Trial.exe
 Start-Sleep -Seconds 60
 cp ./shot.png ../../shot.png
